8e6a5864cdb56b36b19fb5bf9e02414687f7c5d2
Max Wang <mwang@1080agile.com>
2020-04-20 20:21:13 +1000

    add missing file

---
+++ src/containers/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import Burger from "../components/Burger/Burger";
+
+const burgerBuilder = () => (
+  <Fragment>
+    <Burger />
+    <div>Build Controls</div>
+  </Fragment>
+);
+
+export default burgerBuilder;
